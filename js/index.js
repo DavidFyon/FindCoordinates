f02f206ac83b2df7921b00fbafeebddcd97dcfdc
David FYON <davidfyon@gmail.com>
2016-03-26 14:35:10 +0100

    Ajout du Javascript pour connaitre les coordonnées du clic

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 (function() {
   $("html").on("mousedown", function(e) {
-    alert('toto');
+    $("i").addClass("active").css("left", e.pageX).css("top", e.pageY);
+    return $("span").html("X:" + e.pageX + "px  Y:" + e.pageY);
+  });
+
+  $("i").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+    return $("i").removeClass("active");
   });
 
 }).call(this);
